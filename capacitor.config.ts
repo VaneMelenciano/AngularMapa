1b6ffd388a79c8e89f2472b44a74adb78e2b0fb3
VaneMelenciano <vanemelenciano@gmail.com>
2021-12-06 19:00:51 -0600

    aplicación

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'angular-mapa',
+  webDir: 'dist/AngularMapa',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularMapa';
+  lat = 22.78421006858286;
+  lng = -102.61571917405792;
+}
